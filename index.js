bd3d338ef52338feb731396cf8089d997d5ab3c1
Gurun Nevada Dharan <gurunnevada@outlook.com>
2023-05-15 06:20:08 +0700

    push 7

--- index.js
+++ index.js
@@ -6,31 +6,37 @@ require('dotenv').config();
 // OpenAI
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
- apiKey: process.env.API_KEY,
+ apiKey: process.env.API_KEY
 });
 const openai = new OpenAIApi(configuration);
 
 const path = require('path');
 
+const port = 8080;
+
 // Define route for generating text
 app.get('/generate-text', async (req, res) => {
  // Get prompt from query string
- const prompt = 'Who are you?' || req.query.prompt;
+ const prompt = req.query.prompt || 'Hello, what is your name?';
 
- // Set up GPT-3 parameters
+ // Set up GPT-3.5 parameters
  const completion = await openai.createChatCompletion({
    model: "gpt-3.5-turbo",
    temperature: 1,
    max_tokens: 2048,
    messages: [{role: "user", content: prompt}]
  })
+ console.log(completion);
  const text = completion.data.choices[0].message.content.trim();
  res.send(text);
 });
 
-// Start server
-const port = process.env.PORT || 3000;
+// Serve HTML file
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname, 'index.html'));
+});
 
+// Start server
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
