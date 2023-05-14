5a255a3a0eeaf884329be8baa961b0e88a29ace0
Gurun Nevada Dharan <gurunnevada@outlook.com>
2023-05-15 06:17:25 +0700

    push 6

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/generate-text', async (req, res) => {
 });
 
 // Start server
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
